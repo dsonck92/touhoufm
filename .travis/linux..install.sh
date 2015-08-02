e2ae36162b513c42e4429026b22a5e9ded81048c
Daniel Sonck <daniel@sonck.nl>
2015-08-02 14:04:24 +0200

    Qt5 from source

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,3 +1,20 @@
-PACKAGES="qtbase5-dev qtmultimedia5-dev qtwebsockets5-dev"
+PACKAGES="libasound2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev"
 
 sudo apt-get install -y $PACKAGES
+
+sudo apt-get build-dep -y qt5-default
+
+# Get Qt5
+git clone git://code.qt.io/qt/qt5.git
+LASTDIR=$(pwd)
+
+
+  cd qt5
+  git checkout 5.4
+  perl init-repository
+  ./configure -developer-build -opensource -nomake examples -nomake tests
+  make -j2
+
+  make install
+
+cd $LASTDIR
