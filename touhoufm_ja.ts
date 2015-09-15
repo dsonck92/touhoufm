7c615c5ab3d417b008e5e0a3550e47d82b0a94ec
Rui <Rui914@users.noreply.github.com>
2015-09-16 01:52:25 +0900

    Update Japanese translation

--- touhoufm_ja.ts
+++ touhoufm_ja.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="report.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>ダイアログ</translation>
     </message>
     <message>
         <location filename="report.ui" line="20"/>
         <source>Reason</source>
-        <translation type="unfinished"></translation>
+        <translation>理由</translation>
     </message>
 </context>
 <context>
@@ -19,147 +19,147 @@
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Awful</source>
-        <translation type="unfinished"></translation>
+        <translation>最低</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Terrible</source>
-        <translation type="unfinished"></translation>
+        <translation>とても悪い</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Bad</source>
-        <translation type="unfinished"></translation>
+        <translation>悪い</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Neutral</source>
-        <translation type="unfinished"></translation>
+        <translation>普通</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Good</source>
-        <translation type="unfinished"></translation>
+        <translation>良い</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Nice</source>
-        <translation type="unfinished"></translation>
+        <translation>とても良い</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="59"/>
         <source>Awesome</source>
-        <translation type="unfinished"></translation>
+        <translation>最高</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="82"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>再生</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="83"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>停止</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="85"/>
         <source>Report</source>
-        <translation type="unfinished"></translation>
+        <translation>レポート</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="86"/>
         <source>Skip</source>
-        <translation type="unfinished"></translation>
+        <translation>スキップ</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="88"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>ログイン</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="90"/>
         <source>Skin</source>
-        <translation type="unfinished"></translation>
+        <translation>スキン</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="92"/>
         <source>Show</source>
-        <translation type="unfinished"></translation>
+        <translation>表示</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="93"/>
         <source>Minimize</source>
-        <translation type="unfinished"></translation>
+        <translation>最小化</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="94"/>
         <source>Hide</source>
-        <translation type="unfinished"></translation>
+        <translation>隠す</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="96"/>
         <source>Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>終了</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="186"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>不明</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="190"/>
         <source>No Media!</source>
-        <translation type="unfinished"></translation>
+        <translation>再生できません!</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="193"/>
         <source>Loading ...</source>
-        <translation type="unfinished"></translation>
+        <translation>読込中...</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="196"/>
         <source>Loaded!</source>
-        <translation type="unfinished"></translation>
+        <translation>読み込み完了!</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="199"/>
         <source>Stalled!</source>
-        <translation type="unfinished"></translation>
+        <translation>停止しました!</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="202"/>
         <source>Buffering...</source>
-        <translation type="unfinished"></translation>
+        <translation>バッファ中...</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="205"/>
         <source>Buffered</source>
-        <translation type="unfinished"></translation>
+        <translation>バッファ完了</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="208"/>
         <source>Done!</source>
-        <translation type="unfinished"></translation>
+        <translation>完了!</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="212"/>
         <source>Invalid Media!</source>
-        <translation type="unfinished"></translation>
+        <translation>無効なメディアです!</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="255"/>
         <source>Rating: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>評価: %1</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="309"/>
         <source>%1 by %2 from %3 &lt;&lt; %4 &gt;&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>%3から%2による%1  &lt;&lt; %4 &gt;&gt;</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="319"/>
         <source>Now Playing:</source>
-        <translation type="unfinished"></translation>
+        <translation>再生中の楽曲:</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="319"/>
@@ -167,33 +167,36 @@
 Artist: %2
 Album: %3
 Circle: %4</source>
-        <translation type="unfinished"></translation>
+        <translation>タイトル: %1
+アーティスト: %2
+アルバム: %3
+サークル: %4</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="505"/>
         <source>Volume: %1%</source>
-        <translation type="unfinished"></translation>
+        <translation>音量: %1%</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="519"/>
         <source>Reporting %1 by %2 from %3 &lt;&lt; %4 &gt;&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>%3から%2による%1をレポート中  &lt;&lt; %4 &gt;&gt;</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="554"/>
         <source>Touhou.FM Radio</source>
         <oldsource>TouhouFM Radio</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>TouHou.FMラジオ</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="554"/>
         <source>Please authorize and click Ok when ready.</source>
-        <translation type="unfinished"></translation>
+        <translation>承認の準備ができたらOKをクリックしてください。</translation>
     </message>
     <message>
         <location filename="touhoufm.cpp" line="563"/>
         <source>TouHou.FM Radio</source>
-        <translation type="unfinished"></translation>
+        <translation>TouHou.FMラジオ</translation>
     </message>
 </context>
 </TS>
